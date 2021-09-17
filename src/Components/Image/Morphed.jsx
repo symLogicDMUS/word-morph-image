9076202186ae158d7a41aa87b9bfbdb4db4bd582
Rathbone, Brian James <brian.rathbone@wsu.edu>
2021-09-16 20:34:22 -0700

    update to paused morphs

--- src/Components/Image/Morphed.jsx
+++ src/Components/Image/Morphed.jsx
@@ -1,22 +1,31 @@
-import React, { useContext, useMemo } from "react";
-import { useStyles } from "../Morphs/Morph.jss";
-import { Typography, useTheme } from "@material-ui/core";
+import React, {useContext, useState} from "react";
 import AppContext from "../../AppContext";
+import {Badge, Typography} from "@material-ui/core";
+import { useStyles } from "../Morphs/Morph.jss";
 
 function Morphed({ children }) {
     const { state, dispatch } = useContext(AppContext);
 
     const classes = useStyles();
 
+    const [badge, setBadge] = useState(false);
+    const toggleBadge = () => setBadge((prevState) => ! prevState)
+
     return (
         <>
             {!!state.dictionary[children] ? (
-                <img
-                    src={state.dictionary[children]}
-                    className={classes.img}
-                    alt={children}
-                />
-            ) : (
+                <>
+                    <img
+                        src={state.dictionary[children]}
+                        className={classes.img}
+                        onClick={toggleBadge}
+                        alt={children}
+                    />
+                    {badge && (
+                        <Badge badgeContent={children} color="secondary" className={classes.badge} />
+                    )}
+                </>
+                ) : (
                 <Typography className={classes.word}>
                     {children + " "}
                 </Typography>
