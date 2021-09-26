01b17909d9ebec96bb033a023ed34befde430ad1
Rathbone, Brian James <brian.rathbone@wsu.edu>
2021-09-26 09:50:01 -0700

    successfully added IconButton with horizontal 'see more' icon, that opens menu, for small screen widths

--- src/Components/MyAppBar/MyAppBar.jsx
+++ src/Components/MyAppBar/MyAppBar.jsx
@@ -1,23 +1,33 @@
-import React, { useContext } from "react";
+import React, {useState} from "react";
 import AppTitle from "./AppTitle";
-import { useTheme } from "@mui/styles";
-import { GitHub, YouTube } from "@mui/icons-material";
+import {useTheme} from "@mui/styles";
+import MyAppBarItems from "./MyAppBarItems";
+import {MoreHoriz, MoreVert, YouTube} from "@mui/icons-material";
 import MenuIcon from "@mui/icons-material/Menu";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
-import { AppBar, Icon, IconButton, Toolbar, Tooltip } from "@mui/material";
-import SignOutButton from "./SignOutButton";
-import AppContext from "../../AppContext";
-import { useStyles } from "./MyAppBar.jss";
+import {AppBar, Button, IconButton, Menu, Toolbar, Tooltip, useMediaQuery} from "@mui/material";
+import {useStyles} from "./MyAppBar.jss";
 
 export function MyAppBar({ handleDrawerToggle }) {
-    const { state, dispatch } = useContext(AppContext);
 
     const classes = useStyles();
 
     const theme = useTheme();
 
+    const sm = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
+        <>
         <AppBar position="fixed" className={classes.appBar}>
             <Toolbar>
                 <IconButton
@@ -28,12 +38,12 @@ export function MyAppBar({ handleDrawerToggle }) {
                     className={classes.menuButton}
                     size="large"
                 >
-                    <MenuIcon />
+                    <MenuIcon/>
                 </IconButton>
-                <AppTitle mode={theme.palette.mode} />
+                <AppTitle mode={theme.palette.mode}/>
                 <Tooltip title={"watch demo"}>
                     <IconButton
-                        style={{ color: "#f48fb1" }}
+                        style={{color: "#f48fb1"}}
                         onClick={() =>
                             (window.location.href =
                                 "https://youtu.be/9lkwUOnjwUw")
@@ -41,40 +51,32 @@ export function MyAppBar({ handleDrawerToggle }) {
                     >
                         <YouTube
                             fontSize={"large"}
-                            style={{ color: "#f48fb1" }}
+                            style={{color: "#f48fb1"}}
                         />
                     </IconButton>
                 </Tooltip>
-                <Tooltip title={"Toggle light/dark theme"}>
-                    <IconButton
-                        onClick={() =>
-                            dispatch({
-                                type: "update-mode",
-                                isDarkMode: !state.isDarkMode,
-                            })
-                        }
-                        size="large"
-                    >
-                        {state.isDarkMode ? (
-                            <Brightness7Icon />
-                        ) : (
-                            <Brightness4Icon />
-                        )}
-                    </IconButton>
-                </Tooltip>
-                <SignOutButton />
-                <Tooltip title="Author's GitHub">
-                    <IconButton
-                        onClick={() =>
-                            (window.location.href =
-                                "https://github.com/symLogicDMUS/word-morph-image")
-                        }
-                        size="large"
-                    >
-                        <GitHub />
-                    </IconButton>
-                </Tooltip>
+                <MyAppBarItems
+                    handleDrawerToggle={handleDrawerToggle}
+                    style={sm ? { display: "none" } : null}
+
+                />
+                <IconButton
+                    onClick={handleMenuOpen}
+                    style={! sm ? { display: "none" } : null}
+
+                >
+                    <MoreHoriz fontSize={"large"} />
+                </IconButton>
+                <Menu
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handleMenuClose}
+                    style={! sm ? { display: "none" } : null}
+                >
+                    <MyAppBarItems handleDrawerToggle={handleDrawerToggle} />
+                </Menu>
             </Toolbar>
         </AppBar>
+        </>
     );
 }
---
+++ src/Components/MyAppBar/MyAppBarItems.jsx
@@ -0,0 +1,52 @@
+import React, {useContext} from "react";
+import {useTheme} from "@mui/styles";
+import AppContext from "../../AppContext";
+import SignOutButton from "./SignOutButton";
+import {GitHub} from "@mui/icons-material";
+import {IconButton, Tooltip} from "@mui/material";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+
+function MyAppBarItems(props) {
+    const {handleDrawerToggle, ...other} = props;
+
+    const { state, dispatch } = useContext(AppContext);
+
+    const theme = useTheme();
+
+    return (
+        <div {...other}>
+            <Tooltip title={"Toggle light/dark theme"}>
+                <IconButton
+                    onClick={() =>
+                        dispatch({
+                            type: "update-mode",
+                            isDarkMode: !state.isDarkMode,
+                        })
+                    }
+                    size="large"
+                >
+                    {state.isDarkMode ? (
+                        <Brightness7Icon/>
+                    ) : (
+                        <Brightness4Icon/>
+                    )}
+                </IconButton>
+            </Tooltip>
+            <SignOutButton/>
+            <Tooltip title="Author's GitHub">
+                <IconButton
+                    onClick={() =>
+                        (window.location.href =
+                            "https://github.com/symLogicDMUS/word-morph-image")
+                    }
+                    size="large"
+                >
+                    <GitHub/>
+                </IconButton>
+            </Tooltip>
+        </div>
+    )
+}
+
+export default MyAppBarItems;
